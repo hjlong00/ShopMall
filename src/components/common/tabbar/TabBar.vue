68e5716e650aa537ab01b63b2fa09dc4fa24ecb8
hjlong <hjlong2@outlook.com>
2021-08-07 18:04:57 +0800

    0807

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="tab-bar">
+     <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name:'TabBar'
+}
+</script>
+
+<style scoped>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  /* 生成绝对定位的元素，相对于浏览器窗口进行定位。
+   * 元素的位置通过 "left", "top", "right" 以及 "bottom" 属性进行规定。
+   */
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  box-shadow: 0 -1px 1px rgba(134, 132, 132, 0.192);
+}
+
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="#d81e06">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/home_active.svg"
+          alt=""
+        />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#d81e06">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/category_active.svg"
+          alt=""
+        />
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#d81e06">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcar.svg" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/shopcar_active.svg"
+          alt=""
+        />
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="#d81e06">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/profile_active.svg"
+          alt=""
+        />
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/tabbar/TabBar'
+import TabBarItem from 'components/tabbar/TabBarItem.vue'
+
+export default {
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+</style>
