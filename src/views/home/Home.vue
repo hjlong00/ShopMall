3fcd3612992ec5169d5951975a431f586b9439a0
hjlong <hjlong2@outlook.com>
2021-08-08 12:10:59 +0800

    change something

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-首页 adfa asd a 
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-  export default {
+import NavBar from 'components/common/navbar/NavBar.vue'
 
-  }
+export default {
+  components: {
+    NavBar
+  },
+
+}
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color:white;
+  font-weight: 700;
+}
 </style>
+
